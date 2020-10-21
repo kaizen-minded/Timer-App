2ef297f0e2abc83e046f51d66af2a32eb7e2cdf7
kaizen-minded <melvin3ezell@gmail.com>
2020-10-21 11:56:05 -0400

    finish timer

--- main.js
+++ main.js
@@ -1,61 +1,22 @@
-class Timer {
-  constructor(durationInput, playBtn, pauseBtn, callbacks) {
-    this.durationInput = durationInput;
-    this.playBtn = playBtn;
-    this.pauseBtn = pauseBtn;
-    if (callbacks) {
-      this.onStart = callbacks.onStart;
-      this.onTick = callbacks.onTick;
-      this.onComplete = callbacks.onComplete;
-    }
-
-    this.playBtn.addEventListener("click", this.start);
-    this.pauseBtn.addEventListener("click", this.pause);
-  }
-
-  start = () => {
-    if (this.onStart) {
-      this.onStart();
-    }
-    this.tick();
-    this.interval = setInterval(this.tick, 1000);
-  };
-  pause = () => {
-    clearInterval(this.interval);
-  };
-
-  tick = () => {
-    if (this.timeRemaining <= 0) {
-      if (this.onComplete) {
-        this.onComplete();
-      }
-      this.pause();
-    } else {
-      if (this.onTick) {
-        this.onTick();
-      }
-      this.timeRemaining = this.timeRemaining;
-    }
-  };
-
-  get timeRemaining() {
-    return parseFloat(this.durationInput.value);
-  }
-
-  set timeRemaining(time) {
-    this.durationInput.value = --time;
-  }
-}
-
 const durationInput = document.getElementById("duration");
 const startBtn = document.getElementById("start-btn");
 const pauseBtn = document.getElementById("pause-btn");
+const circle = document.querySelector("circle");
+
+const perimeter = circle.getAttribute("r") * 2 * Math.PI;
+circle.setAttribute("stroke-dasharray", perimeter);
+
+let currentOffset = 0;
+let duration;
 const timer = new Timer(durationInput, startBtn, pauseBtn, {
-  onStart() {
-    console.log("timer started");
+  onStart(totalDuration) {
+    duration = totalDuration;
   },
-  onTick() {
-    console.log("tick tok");
+  onTick(timeRemaining) {
+    circle.setAttribute(
+      "stroke-dashoffset",
+      (perimeter * timeRemaining) / duration - perimeter
+    );
   },
   onComplete() {
     console.log("finished");
